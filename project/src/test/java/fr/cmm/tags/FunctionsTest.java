dd6204de831a8853571865a242b3bc05fe6eda4a
Mathilde Matly <mathildematly@orange.fr>
2018-10-11 11:06:14 +0200

    rajout FunctionTest

--- project/src/test/java/fr/cmm/tags/FunctionsTest.java
+++ project/src/test/java/fr/cmm/tags/FunctionsTest.java
@@ -1,5 +1,27 @@
+package fr.cmm.tags;
+
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class FunctionsTest {
+
+    @Test
+    public void showstring() {
+    assertEquals("a", Functions.text("a"));}
+
+    @Test
+    public void espace() {
+        assertEquals("a<br>", Functions.text("a\n"));}
+
+
+    @Test
+    public void symbols() {
+        assertEquals("&amp;a", Functions.text("&a"));}
+
+
+
+
+
+
 
 }
